88842c5924793cd5f03a66ab1511886999a8dc72
Emilio Dalla Torre <emiliodallatorre12@live.com>
2017-08-03 10:14:56 +0200

    Added Wikipedia and Italian dictionary support, changed methods to retrieve data from Web Services.

--- src/com/EmilioDallaTorre/Act.java
+++ src/com/EmilioDallaTorre/Act.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Act {
     public static void Meteo(String dico) {
         Document tempometeo = null;
-        String cercometeo = dico.substring(dico.lastIndexOf(" a ")+3);
+        String cercometeo = dico.substring(dico.lastIndexOf(" a ") + 3);
         try {
             tempometeo = Jsoup.connect("https://www.bing.com/search?q=meteo+" + cercometeo).userAgent("Mozilla").get();
         } catch (IOException e) {
@@ -32,36 +32,58 @@ public class Act {
 
     public static void Latino(String dico) {
         Document significatoolivetti = null;
-        String cercolatino = dico.substring((dico.indexOf("'"), (dico.lastIndexOf("'")));
         try {
+            String cercolatino = dico.substring((dico.indexOf("'")) + 1, (dico.lastIndexOf("'")));
             significatoolivetti = Jsoup.connect("http://www.dizionario-latino.com/dizionario-latino-italiano.php?parola=" + cercolatino).userAgent("Mozilla").get();
+            String divs = significatoolivetti.select(".italiano").text();
+            if (!divs.equals("")) {
+                System.out.println("La parola latina '" + cercolatino + "' secondo il dizionario online di Olivetti, significa '" + divs + "'.");
+            } else {
+                System.out.println("Mi dispiace, non ho trovato un significato alla parola che mi hai chiesto di cercare...");
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (StringIndexOutOfBoundsException f) {
+            System.out.println("Ricorda che, perché io cerchi il significato di una parola, occorre che tu la definisca fra due virgolette!");
         }
-        String divs = significatoolivetti.select(".italiano").text();
-        if (!divs.equals("")) {
-            System.out.println("La parola latina " + cercolatino + " secondo il dizionario online di Olivetti, significa '" + divs + "'.");
-        } else {
-            System.out.println("Mi dispiace, non ho trovato un significato alla parola che mi hai chiesto di cercare...");
+    }
+
+    public static void Italiano(String dico) {
+        Document significatoolivetti = null;
+        String cercoitaliano = dico.substring((dico.indexOf("'")) + 1, (dico.lastIndexOf("'")));
+        try {
+            significatoolivetti = Jsoup.connect("http://www.dizionario-italiano.it/dizionario-italiano.php?parola=" + cercoitaliano).userAgent("Mozilla").get();
+            String divs = significatoolivetti.select(".italiano").text();
+            if (!divs.equals("")) {
+                System.out.println("La parola italiana '" + cercoitaliano + "' secondo il dizionario online di Olivetti, significa '" + divs + "'.");
+            } else {
+                System.out.println("Mi dispiace, non ho trovato un significato alla parola che mi hai chiesto di cercare...");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (StringIndexOutOfBoundsException f) {
+            System.out.println("Ricorda che, perché io cerchi il significato di una parola, occorre che tu la definisca fra due virgolette!");
         }
     }
 
     public static void Wikipedia(String dico) {
         Document significatowikipedia = null;
-        String cercowikipedia = dico.substring(23);
+        String cercowikipedia = dico.substring((dico.indexOf("'")) + 1, (dico.lastIndexOf("'")));
         try {
             significatowikipedia = Jsoup.connect("https://it.wikipedia.org/wiki/" + cercowikipedia.replace(" ", "_")).userAgent("Mozilla").get();
-        } catch (HttpStatusException f) {
+            String divs = significatowikipedia.select("p").text();
+            if (!divs.equals("")) {
+                System.out.println("La ricerca di " + cercowikipedia + " su wikipedia ha restituito il seguente risultato:");
+                System.out.println(divs);
+            } else {
+                System.out.println("Mi dispiace, non ho trovato informazioni su " + cercowikipedia + " su Wikipedia...");
+            }
+        } catch (HttpStatusException e) {
+            System.out.println("Mi dispiace, Wikipedia sembra non avere una voce per '" + cercowikipedia +"'...");
+        } catch (java.io.IOException f) {
             f.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String divs = significatowikipedia.select("p").text();
-        if (!divs.equals("")) {
-            System.out.println("La ricerca di " + cercowikipedia + " su wikipedia ha restituito il seguente risultato:");
-            System.out.println(divs);
-        } else {
-            System.out.println("Mi dispiace, non ho trovato informazioni su " + cercowikipedia + " su Wikipedia...");
+        } catch (StringIndexOutOfBoundsException g) {
+            System.out.println("Ricorda che, perché io cerchi informazioni riguardo a qualcosa, occorre che tu la definisca fra due virgolette!");
         }
     }
 }
--- src/com/EmilioDallaTorre/Classify.java
+++ src/com/EmilioDallaTorre/Classify.java
@@ -4,14 +4,16 @@ public class Classify {
     public static void classify(String dico) {
         Boolean Wikipedia = dico.contains("Wikipedia");
         Boolean Meteo = dico.contains("tempo") || dico.contains("meteo");
-        Boolean Latino = dico.contains("latino");
-
+        Boolean Latino = dico.contains("significa") && dico.contains("latino") || dico.contains("latino");
+        Boolean Timer = dico.contains("significa") || dico.contains("italiano") || dico.contains("vuol dire");
         if(Wikipedia) {
             Act.Wikipedia(dico);
         } else if(Meteo) {
             Act.Meteo(dico);
         } else if(Latino) {
             Act.Latino(dico);
+        } else if(Timer) {
+            Act.Italiano(dico);
         }
     }
 }
