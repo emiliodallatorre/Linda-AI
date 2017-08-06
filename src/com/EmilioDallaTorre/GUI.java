125d69e206a9ebd3dfe55176eb8cd78ec9636db3
Emilio Dalla Torre <emiliodallatorre12@live.com>
2017-08-06 16:41:32 +0200

    Modified the GUI class. Still trying to make it work.

--- src/com/EmilioDallaTorre/GUI.java
+++ src/com/EmilioDallaTorre/GUI.java
@@ -8,8 +8,9 @@ public class GUI extends JFrame {
     private JTextField inputArea;
     private JTextArea responseArea;
     private JFrame frame = new JFrame("Linda-AI");
-    private GridLayout layout = new GridLayout(2,2);
+    private GridLayout layout = new GridLayout(2, 2);
     private JPanel panel = new JPanel(layout);
+
     void createWindow() {
         inputArea.setText("Parla con l'intelligenza artificiale Linda!");
         panel.add(responseArea);
@@ -20,15 +21,15 @@ public class GUI extends JFrame {
         frame.setVisible(true);
         inputArea.addActionListener((ActionEvent e) -> Classify.classify(inputArea.getText()));
     }
-    void giveResponse(String risposta) {
-        responseArea.setEditable(false);
-        responseArea.setLineWrap(true);
-        responseArea.append(risposta + '\n');
-        SwingUtilities.invokeLater(new Runnable(){
-            @Override
-            public void run() {
-                responseArea.append(risposta); //DOES NOT CHANGE IN GUI
-            }
-        });
+
+    void giveResponse(final String risposta) {
+        new Thread(SwingUtilities.invokeLater(() -> {
+            responseArea.setEditable(false);
+            responseArea.setLineWrap(true);
+            responseArea.append(risposta + '\n');
+            panel.add(responseArea);
+            revalidate();
+            repaint();
+        })).start();
     }
 }
