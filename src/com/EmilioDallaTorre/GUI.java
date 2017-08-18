1984b2c9026a4a17fd5da21ba16b5ef757987041
Emilio Dalla Torre <emiliodallatorre12@live.com>
2017-08-18 16:00:50 +0200

    Modified the GUI class. Now it works through a TTS that speak the answers.

--- src/com/EmilioDallaTorre/GUI.java
+++ src/com/EmilioDallaTorre/GUI.java
@@ -1,11 +1,20 @@
 package com.EmilioDallaTorre;
 
 import javax.swing.*;
+import java.util.concurrent.Executors;
+import com.sun.speech.freetts.*;
+
 
 public class GUI extends JFrame {
-    private void createWindow() {
-    }
     void giveResponse(String risposta) {
-            System.out.println(risposta);
+        System.out.println(risposta);
+        Executors.newSingleThreadExecutor().execute(() -> {
+            String VOICENAME = "kevin16";
+            Voice voice;
+            VoiceManager voiceManager = VoiceManager.getInstance();
+            voice = voiceManager.getVoice(VOICENAME);
+            voice.allocate();
+            voice.speak(risposta);
+        });
     }
 }
