00976e5057450715a10a09f0b307f0df34b03a4a
Emilio Dalla Torre <emiliodallatorre12@live.com>
2017-11-18 19:09:53 +0100

    Removed completely the vocal input.

--- src/com/EmilioDallaTorre/GUI.java
+++ src/com/EmilioDallaTorre/GUI.java
@@ -1,20 +1,9 @@
 package com.EmilioDallaTorre;
 
-import com.sun.speech.freetts.*;
-
 import javax.swing.*;
-import java.util.concurrent.Executors;
 
 public class GUI extends JFrame {
     void giveResponse(String risposta) {
         System.out.println(risposta);
-        Executors.newSingleThreadExecutor().execute(() -> {
-            String VOICENAME = "kevin16";
-            Voice voice;
-            VoiceManager voiceManager = VoiceManager.getInstance();
-            voice = voiceManager.getVoice(VOICENAME);
-            voice.allocate();
-            voice.speak(risposta);
-        });
     }
 }
